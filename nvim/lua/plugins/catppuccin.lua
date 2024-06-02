8b0a5289bc0d3db712a356c75e0255f62d735bd4
Andrei Manu <896868@stud.unive.it>
2024-06-02 18:38:18 +0200

    none-ls configuration and auto-install for treesitter enabled

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -4,10 +4,39 @@ return
     name = "catppuccin", 
     priority = 1000,
     config = function()
-        vim.cmd.colorscheme "catppuccin"
         require('catppuccin').setup({
-            base = "#000000" -- fix
+            flavour = "mocha", -- latte, frappe, macchiato, mocha
+            background = { -- :h background
+                light = "latte",
+                dark = "mocha",
+            },
+            transparent_background = false,
+            term_colors = true,
+            styles = {
+                comments = { "italic" },
+                conditionals = { "italic" },
+                loops = {},
+                functions = {},
+                keywords = {},
+                strings = {},
+                variables = {},
+                numbers = {},
+                booleans = {},
+                properties = {},
+                types = {},
+                operators = {},
+            },
+            color_overrides = {
+                all = {
+                    base = "#000000", -- Fix
+                },
+            },
+            custom_highlights = {},
+            integrations = {
+                -- Add integrations if needed
+            },
         })
-   end
+        vim.cmd.colorscheme "catppuccin"
+    end
 }
 
