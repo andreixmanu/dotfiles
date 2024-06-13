cf63c5bdc485e5e154c5a8b395145c76b576968c
andreixmanu <896868@stud.unive.it>
2024-06-13 14:30:24 +0200

    added README and installation script (untested)

---
+++ README.md
@@ -0,0 +1,43 @@
+# andreixmanu dotfiles
+
+## Installation guide
+
+1. Install git on your sistem
+```shell
+sudo pacman -S git # for arch based
+sudo apt install git # for debian based
+sudo dnf install git # for fedora based 
+```
+
+2. *Inside your home folder* clone the repo
+```shell
+git clone https://github.com/andreixmanu/dotfiles . && cd dotfiles
+```
+This will create a folder in your home directory called 'dotfiles'.
+
+3. Grant the installation script the execution permission.
+```shell
+chmod +x install.sh
+```
+4. Execute the installation script
+```shell
+./install.sh
+```
+
+## General Information
+
+### List of tools:
+
+- [Alacritty](https://alacritty.org/) : standard terminal
+- [Kitty](https://sw.kovidgoyal.net/kitty/) : hyperland terminal
+- [Neovim](https://neovim.io/) : text editor (plugins included)
+- [Btop](https://github.com/aristocratos/btop) : terminal based resources monitor
+- [Neofetch](https://github.com/dylanaraps/neofetch) : command-line system information tool
+- [Tmux](https://github.com/tmux/tmux) : terminal multiplexer
+
+### Hyperland-only tools
+These tools only work if you are using Hyperland WM. If you are not using Hyperland, the script will automatically remove these tools from the dotfiles folder. If you want to keep them, answer 'y' when prompted.
+
+- [hypr](https://hyprland.org/) : hyperland configuration
+- [waybar](https://github.com/Alexays/Waybar) : status bar for wayland DE
+- [rofi](https://github.com/davatorium/rofi) : A window switcher, Application launcher and dmenu replacement.
---
+++ install.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+distro=""
+distro_name=""
+username=$(whoami)
+
+echo "
+                 _          _                                  
+                | |        (_)                                 
+  __ _ _ __   __| |_ __ ___ ___  ___ __ ___   __ _ _ __  _   _ 
+ / _\` | '_ \ / _\` | '__/ _ \ \ \/ / '_ \` _ \ / _\` | '_ \| | | |
+| (_| | | | | (_| | | |  __/ |>  <| | | | | | (_| | | | | |_| |
+ \__,_|_| |_|\__,_|_|  \___|_/_/\_\_| |_| |_|\__,_|_| |_|\__,_|
+                                                               
+"
+
+echo "
+                 _       _    __ _ _           
+                | |     | |  / _(_) |          
+              __| | ___ | |_| |_ _| | ___  ___ 
+             / _\` |/ _ \| __|  _| | |/ _ \/ __|
+            | (_| | (_) | |_| | | | |  __/\__ \\
+             \__,_|\___/ \__|_| |_|_|\___||___/
+
+"
+
+sleep 1
+
+echo "Hi $username, this script will install the necessary packages for the development environment"
+echo "Are you sure you want to continue? (Y/n)"
+read -r response
+if [ "$response" == "n" -o "$response" == "N" ]; then
+  echo "Exiting..."
+  exit 1
+fi
+
+# Get the distro name
+if [ -f /etc/os-release ]; then
+  # Read the contents of os-release into a variable
+  source /etc/os-release
+
+  # Print the PRETTY_NAME variable (more user-friendly name)
+  distro="$ID"
+  distro_name="$PRETTY_NAME"
+  # echo "Distro: $distro"
+fi
+
+# create an array of package names
+packages_basic=(git neovim tmux zsh curl stow wget unzip rust neofetch btop kitty alacritty)
+packages_hypr=(git neovim tmux zsh curl stow wget unzip rust neofetch btop alacritty kitty hypr rofi waybar)
+
+echo "Are you using Hyperland desktop environment? (Y/n)"
+read -r response
+
+if [ "$response" == "n" -o "$response" == "N" ]; then
+
+  echo "Installing basic packages for $distro_name"
+  packages=("${packages_basic[@]}")
+
+  sleep 1 
+  
+  sudo pacman -S --noconfirm --needed "${packages[@]}"
+
+else
+
+  echo "Installing packages for Hyperland"
+  packages=("${packages_hypr[@]}")
+
+fi
+
+# create a backup of the existing dotfiles in the .config directory
+echo "Creating a backup of the existing dotfiles of the .config folder"
+if [ -d "$HOME/.config" ]; then
+  mv "$HOME/.config" "$HOME/.config.bak"
+  mkdir "$HOME/.config"
+fi
+echo "Backup created -> $HOME/.config.bak"
+echo "If you have any important files in the .config folder, please move them to the new .config directory"
+
+sleep 1
+# if the user is not in hyperland, remove some folders from the dotfiles directory
+if [ "$response" == "n" -o "$response" == "N" ]; then
+  echo "As you are not using Hyperland, removing useless folders from the dotfiles directory"
+  rm -rf "$HOME/dotfiles/hypr"
+  rm -rf "$HOME/dotfiles/rofi"
+  rm -rf "$HOME/dotfiles/waybar"
+  rm -rf "$HOME/dotfiles/waybar-bak"
+fi
+
+# create symlinks between the dotfiles and the .config directory
+cd "$HOME/dotfiles"
+stow --adopt .
+
+echo "Created symlinks between the dotfiles and the .config directory"
+echo "Every time you make changes to the dotfiles, you can run the 'stow --adopt .' command in the dotfiles directory to update the symlinks"
+
